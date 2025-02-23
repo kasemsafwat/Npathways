a50834f244b1151fcd0fb95adbb5154535bb49f3
MoodyEG <moodyeg2011@yahoo.com>
2025-02-23 19:15:39 +0200

    Added a placeholder working group chat

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { Server } from 'socket.io';
+import cors from 'cors';
 import connectDB from './config/db.js';
 
 dotenv.config();
@@ -8,14 +10,33 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Server is working');
 });
 
 const port = process.env.PORT || 3000;
-const host = process.env.HOST || 'localhost';
+const host = process.env.HOST || 'http://localhost';
 
 const server = app.listen(port, () => {
   console.log(`Server is running on ${host}:${port}`);
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+let userNumber = 0;
+io.on('connection', (socket) => {
+  console.log(`User #${++userNumber} connected`);
+  socket.on('sendMessage', (message) => {
+    console.log(message);
+    io.emit('messageSent', message);
+  });
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
 });
