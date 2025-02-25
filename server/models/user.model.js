d2c8f4aba36c25d3c169a769355a4601b67beba7
sohailaabdelazeem <sohailaabdelazeem863@gmail.com>
2025-02-25 05:09:49 +0200

    .

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,39 +1,3 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
-import bcrypt from 'bcrypt';
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    trim: true,
-    required: true,
-    minlength: 8,
-  },
-  // token:[{
-  //     type:String,
-  //     expires:"2d",
-  //     trim:true
-  // }]
-  tokens: {
-    type: [String],
-    default: [],
-  },
-});
 import mongoose from "mongoose";
 const Schema=mongoose.Schema
 import bcrypt from "bcrypt"
