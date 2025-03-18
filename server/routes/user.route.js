cc4320ef66fb4ea28641e633c3715b81248204d2
sohailaabdelazeem <sohailaabdelazeem863@gmail.com>
2025-03-18 06:45:39 +0200

    Added image upload to user using multer

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,19 +1,27 @@
-import express from 'express';
-import userController from '../controllers/user.controller.js';
+import express from "express";
+import userController from "../controllers/user.controller.js";
+import { userUpload } from "../middleware/userImage.middleware.js";
 import {
   newUserValidation,
   loginValidation,
-} from '../middleware/user.middleware.js';
-import { authentication } from '../middleware/auth.middleware.js';
+} from "../middleware/user.middleware.js";
+import { authentication } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post('/signup', newUserValidation, userController.newUser);
-router.post('/login', loginValidation, userController.login);
-router.get('/all', authentication, userController.getAllUsers);
-router.get('/search', authentication, userController.searchUser);
-router.get('/:id', authentication, userController.getUserById);
+router.post("/signup", newUserValidation, userController.newUser);
+router.post("/login", loginValidation, userController.login);
+router.get("/all", authentication, userController.getAllUsers);
+router.get("/search", authentication, userController.searchUser);
+router.get("/:id", authentication, userController.getUserById);
 
+// changUserImage 
+router.post(
+  "/changUserImage/:id",
+  userUpload.single("image"),
+  authentication,
+  userController.changUserImage
+);
 // ///////////////////
 
 // router.delete("/:id",authentication,userController.deleteUser)
