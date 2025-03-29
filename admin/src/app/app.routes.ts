4edd8c96e89c7291574febd9f88295387edad849
hazemr2121 <ha.ahmed8996@gmail.com>
2025-03-29 23:55:13 +0200

    Feat: angular init + routes pages + navbar

--- admin/src/app/app.routes.ts
+++ admin/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
+import { UserManagementComponent } from './pages/user-management/user-management.component';
+import { CourseManagementComponent } from './pages/course-management/course-management.component';
+import { EnrollmentManagementComponent } from './pages/enrollment-management/enrollment-management.component';
+import { ReportsAnalyticsComponent } from './pages/reports-analytics/reports-analytics.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'admin-dashboard', pathMatch: 'full' },
+  { path: 'admin-dashboard', component: AdminDashboardComponent },
+  { path: 'user-management', component: UserManagementComponent },
+  { path: 'course-management', component: CourseManagementComponent },
+  { path: 'enrollment-management', component: EnrollmentManagementComponent },
+  { path: 'reports-analytics', component: ReportsAnalyticsComponent },
+  // { path: '**', component:NotFoundComponent },
+];
---
+++ admin/src/app/pages/enrollment-management/enrollment-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enrollment-management',
+  imports: [],
+  templateUrl: './enrollment-management.component.html',
+  styleUrl: './enrollment-management.component.css'
+})
+export class EnrollmentManagementComponent {
+
+}
