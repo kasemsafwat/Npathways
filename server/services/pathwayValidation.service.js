80a9c0e353dc2c323f568b1089d2ff75ffbe9a49
sohailaabdelazeem <sohailaabdelazeem863@gmail.com>
2025-04-01 22:56:14 +0200

    Fix pathway validation and update courses handling

--- server/services/pathwayValidation.service.js
+++ server/services/pathwayValidation.service.js
@@ -1,24 +1,39 @@
 import Joi from "joi";
 export let pathwaySchema = Joi.object({
-    name: Joi.string()
-        .trim()
-        .required()
-        .messages({
-            "string.empty": "Name is required and cannot be empty.",
-            "any.required": "Name is a required field.",
-        }),
-        courses: Joi.array()
-        .items(Joi.string().pattern(/^[0-9a-fA-F]{24}$/)) 
-        .custom((value, helpers) => {
-            const uniqueCourses = [...new Set(value)];  
-            if (uniqueCourses.length !== value.length) {
-                return helpers.error("any.unique");
-            }
-            return value;
-        })
-        .messages({
-            "array.base": "Courses must be an array.",
-            "string.pattern.base": "Each course ID must be a valid MongoDB ObjectId.",
-            "any.unique": "Each course must be unique in the pathway.",
-        }),
+  name: Joi.string().trim().required().messages({
+    "string.empty": "Name is required and cannot be empty.",
+    "any.required": "Name is a required field.",
+  }),
+  description: Joi.string().optional(),
+  courses: Joi.array()
+    .items(Joi.string().pattern(/^[0-9a-fA-F]{24}$/))
+    .custom((value, helpers) => {
+      const uniqueCourses = [...new Set(value)];
+      if (uniqueCourses.length !== value.length) {
+        return helpers.error("any.unique");
+      }
+      return value;
+    })
+    .messages({
+      "array.base": "Courses must be an array.",
+      "string.pattern.base": "Each course ID must be a valid MongoDB ObjectId.",
+      "any.unique": "Each course must be unique in the pathway.",
+    }),
+});
+
+export let addCourseSchema = Joi.object({
+  courses: Joi.array()
+    .items(Joi.string().pattern(/^[0-9a-fA-F]{24}$/))
+    .custom((value, helpers) => {
+      const uniqueCourses = [...new Set(value)];
+      if (uniqueCourses.length !== value.length) {
+        return helpers.error("any.unique");
+      }
+      return value;
+    })
+    .messages({
+      "array.base": "Courses must be an array.",
+      "string.pattern.base": "Each course ID must be a valid MongoDB ObjectId.",
+      "any.unique": "Each course must be unique in the pathway.",
+    }),
 });
