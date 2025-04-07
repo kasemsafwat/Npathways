85b3805ee5069114adbbf4bcced690cace2a39c1
hazemr2121 <ha.ahmed8996@gmail.com>
2025-04-07 23:36:17 +0200

    exam service add authentication

--- admin/src/app/services/exam.service.ts
+++ admin/src/app/services/exam.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { HttpHeadersService } from './http-headers.service';
 export interface ExamPayload {
   name: string;
   timeLimit: number;
@@ -37,46 +38,61 @@ export interface Exam {
 @Injectable({
   providedIn: 'root',
 })
-
 export class ExamService {
   private apiUrl = 'http://localhost:5024/api/exam';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private headerService: HttpHeadersService
+  ) {}
 
   getExams(): Observable<Exam[]> {
-    return this.http.get<Exam[]>(this.apiUrl);
+    return this.http.get<Exam[]>(this.apiUrl, {
+      headers: this.headerService.getAuthHeaders(),
+    });
   }
 
   getExamById(id: string): Observable<Exam> {
     const url = `${this.apiUrl}/${id}`;
-    return this.http.get<Exam>(url);
+    return this.http.get<Exam>(url, {
+      headers: this.headerService.getAuthHeaders(),
+    });
   }
 
   createExam(examData: ExamPayload): Observable<Exam> {
     const url = `${this.apiUrl}/createExam`;
-    return this.http.post<Exam>(url, examData);
+    return this.http.post<Exam>(url, examData, {
+      headers: this.headerService.getAuthHeaders(),
+    });
   }
 
   updateExam(id: string, examData: ExamPayload): Observable<Exam> {
     const url = `${this.apiUrl}/updateExam/${id}`;
-    return this.http.put<Exam>(url, examData);
-    // Example URL: PUT http://localhost:5024/api/exam/updateExam/{id}
+    return this.http.put<Exam>(url, examData, {
+      headers: this.headerService.getAuthHeaders(),
+    });
   }
 
   deleteExam(id: string): Observable<void> {
     const url = `${this.apiUrl}/deleteExam/${id}`;
-    return this.http.delete<void>(url);
-    // Example URL: DELETE http://localhost:5024/api/exam/deleteExam/{id}
+    return this.http.delete<void>(url, {
+      headers: this.headerService.getAuthHeaders(),
+    });
   }
-
 
   uploadQuestionsSheet(file: File, examId: string): Observable<any> {
     const formData = new FormData();
     formData.append('file', file, file.name);
-    return this.http.post(`${this.apiUrl}/uploadQuestionsSheet/${examId}`, formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    });
+
+    const headers = this.headerService.getAuthHeaders();
+    const uploadHeaders = headers;
+
+    return this.http.post(
+      `${this.apiUrl}/uploadQuestionsSheet/${examId}`,
+      formData,
+      {
+        headers: uploadHeaders,
+      }
+    );
   }
 }
